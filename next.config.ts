bd19a7856f00ebcffd8d0c94c490f19fe22cc858
Thomas <thomas@fontes.ca>
2026-09-03 13:04:29 -0300

    Redirect URL

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,30 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/termos",
+        destination: "/#termos",
+        permanent: false,
+      },
+      {
+        source: "/privacidade",
+        destination: "/#privacidade",
+        permanent: false,
+      },
+      {
+        source: "/termos-de-uso",
+        destination: "/#termos",
+        permanent: false,
+      },
+      {
+        source: "/politica-de-privacidade",
+        destination: "/#privacidade",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
